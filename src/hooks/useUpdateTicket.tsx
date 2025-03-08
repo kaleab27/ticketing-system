25afdd84f08d2122193b8226af978d07f3b48abe
kaleab tadesse <kaleabtadesse49@gmail.com>
2025-03-08 10:01:28 +0300

    remove hardcoded token

--- src/hooks/useUpdateTicket.tsx
+++ src/hooks/useUpdateTicket.tsx
@@ -2,8 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export default function useUpdateTicket() {
   const queryClient = useQueryClient();
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3Y2IyNDY4ODY2ZjFiZWE3MWFhYWNlMiIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTc0MTM2NjM4NiwiZXhwIjoxNzQxMzY5OTg2fQ.hxH93UbitJce3IBQpjr-oxF2OiLks67k0YP6Jo2oXlg"; //localStorage.getItem("token");
+  const token = localStorage.getItem("token");
   const url = import.meta.env.VITE_BACKEND_ENDPOINT;
 
   return useMutation({
